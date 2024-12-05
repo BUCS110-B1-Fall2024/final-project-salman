7e3f03f2e7c6da2b956ad51d44334e2aa79c0643
Salman-Rahman-Bing <srahma62@binghamton.edu>
2024-12-05 14:53:59 -0500

    Done with Project

--- final_project/src/food.py
+++ final_project/src/food.py
@@ -1,18 +1,6 @@
 class Food:
     def __init__(self, x, y):
-        """
-        Initializes the Food object.
-        Args:
-            x (int): x-coordinate of the food.
-            y (int): y-coordinate of the food.
-        """
         self.position = (x, y)
 
     def relocate(self, x, y):
-        """
-        Changes the food's position.
-        Args:
-            x (int): New x-coordinate.
-            y (int): New y-coordinate.
-        """
         self.position = (x, y)
